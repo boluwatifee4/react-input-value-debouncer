eb210702893ad89fb7025ede7a9b8c3c8b84024e
boluwatifee4 <boluwatifee4@gmail.com>
2023-01-30 18:40:23 +0000

    fixes

---
+++ dist/index.d.ts
@@ -0,0 +1,21 @@
+import React, { ChangeEventHandler } from 'react';
+
+type Props = {
+    debounceTime?: any;
+    className?: string;
+    style?: React.CSSProperties;
+    type?: string;
+    value?: string;
+    name?: string;
+    id?: string;
+    label?: string;
+    error?: boolean;
+    message?: string;
+    success?: boolean;
+    disabled?: boolean;
+    placeholder?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+};
+declare function DebouncerInput({ debounceTime, className, style, placeholder, type, value, onChange, name }: Props): JSX.Element;
+
+export { DebouncerInput };
